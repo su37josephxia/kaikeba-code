ac365650f3bc61556f6a4d431e33d1450356b7ef
xiaran <xiaran@polomi.com>
2019-06-10 16:30:40 +0800

    add 03 front end

---
+++ node/03/cors/front-end/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  async mounted(){
+    const res = await axios.get('/api/user')
+    console.log('res,',res)
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
