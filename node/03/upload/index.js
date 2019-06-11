ab32c6397f738986be6c766382569732c943229c
xiaran <xiaran@polomi.com>
2019-06-11 11:03:18 +0800

    update upload

--- node/03/upload/index.js
+++ node/03/upload/index.js
@@ -1,21 +1,43 @@
 const http = require('http')
 const fs = require('fs')
 const path = require('path')
+const chunk = []
+let size = 0
 const server = http.createServer((request, response) => {
     const { pathname } = require('url').parse(request.url)
+
     if (pathname === '/upload') {
+        console.log('uuuu....')
         const fileName = request.headers['file-name'] ? request.headers['file-name'] : 'abc.png'
         const outputFile = path.resolve(__dirname, fileName)
         const fis = fs.createWriteStream(outputFile)
 
-        // request.on('data', data => fis.write(data))
-        // request.on('finish', () => {
+        // request.on('data', data => {
+        //     console.log('data:',data)
+        //     fis.write(data)
+        // })
+        // request.on('end', () => {
         //     fis.end()
         //     response.end()
         // })
 
-        request.pipe(fis)
-        response.end()
+        // request.on('data',data => {
+        //     chunk.push(data)
+        //     size += data.length
+        //     console.log('data:',size)
+        // })
+        // request.on('end',() => {
+        //     console.log('end...')
+        //     const buffer = Buffer.concat(chunk,size)
+
+        //     size = 0
+        //     fs.writeFileSync(outputFile,buffer)
+        //     response.end()
+        // })
+
+
+        // request.pipe(fis)
+        // response.end()
 
     } else {
         const filename = pathname === '/' ? 'index.html' : pathname.substring(1)
