80aa2009d725608f4409b65f09f589892f1b5c38
xiaran <xiaran@polomi.com>
2019-06-17 15:35:28 +0800

    update

--- node/04/mysql/pool/mysql.js
+++ node/04/mysql/pool/mysql.js
@@ -9,23 +9,28 @@
         password: "example", // 修改为你的密码
         database: "shop", // 请确保数据库存在
         connectionLimit : 5,
-    };
-    // create the connection
+    }
 
+    // 设置连接池
     const pool = await mysql.createPool(cfg)
 
-
-
+    // 非连接池
     const query = async () => {
-        // const connection = await mysql.createConnection(cfg)
+        const connection = await mysql.createConnection(cfg)
+        const [rows, fields] = await connection.execute(`SELECT * FROM users`)
+        console.log('select:', rows)
+        connection.destroy()
+    }
 
-        // 连接池
+    // 连接池
+    const query = async () => {
+        
         const connection = await pool.getConnection()
         const [rows, fields] = await connection.execute(`SELECT * FROM users`)
         console.log('select:', rows)
-        // connection.release()
-
+        connection.release()
     }
+
     const { asyncFun } = require('./async')
     await asyncFun(query, 20, 500)
 
