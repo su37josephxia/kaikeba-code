0beb4c150ac0f83934a7b590409dc3d65b596b1c
xiaran <xiaran@polomi.com>
2019-11-05 18:46:53 +0800

    bodypaser add promise

--- node/03/bodyparser/index.js
+++ node/03/bodyparser/index.js
@@ -1,32 +1,44 @@
 const Koa = require('koa')
 const app = new Koa()
 const bodyParser = require('koa-bodyparser')
-app.use(require('koa-static')(__dirname + '/'))
-app.use(bodyParser())
+
+// app.use(bodyParser())
 const router = require('koa-router')()
-// app.use((ctx, next) => {
-//     const req = ctx.request.req
-//     let reqData = [];
-//     let size = 0;
-//     req.on('data', data => {
-//         console.log('>>>req on',data);
-//         reqData.push(data);
-//         size += data.length;
-//     });
-//     req.on('end', function () {
-//         console.log('end')
-//         const data = Buffer.concat(reqData, size);
-//         console.log('data:', size, data.toString())
-//     });
-//     next();
-// });
+
+app.use(require('koa-static')(__dirname + '/'))
+
+
+app.use(async (ctx, next) => {
+    console.log('body-parser.....')
+    const req = ctx.request.req
+    let reqData = [];
+    let size = 0;
+    await new Promise((resolve,reject) => {
+        req.on('data', data => {
+            console.log('>>>req on',data);
+            reqData.push(data);
+            size += data.length;
+        });
+        req.on('end', function () {
+            console.log('end')
+            const data = Buffer.concat(reqData, size);
+            console.log('data:', size, data.toString())
+            ctx.request.body = data.toString()
+            resolve()
+        });
+    })
+    await next();
+});
+
 router.post('/add', async (ctx, next) => {
 
     console.log('body', ctx.request.body)
     ctx.body = ctx.request.body
+    next()
 })
 
 app.use(router.routes())
+
 
 
 app.listen(3000)
