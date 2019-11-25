590a1f40449db60302222587e7f9b26ff2d54041
xiaran <xiaran@polomi.com>
2019-11-25 15:20:27 +0800

    增加耗时日志

--- node/04/mysql/pool/async.js
+++ node/04/mysql/pool/async.js
@@ -5,6 +5,7 @@ const sleep = delay => new Promise(resolve => setTimeout(resolve, delay))
 const asyncFun = async (fun, curMax = 4, sum = 200) => {
     let num = 0
     let curNum = 0
+    console.time('总耗时');
     console.log('beginTime:' + new Date().toLocaleString())
     const result = []
     while (num !== sum) {
@@ -22,12 +23,13 @@ const asyncFun = async (fun, curMax = 4, sum = 200) => {
         }
     }
     console.log('endTime:' + new Date().toLocaleString())
+    console.timeEnd('总耗时');
 }
-module.exports = {asyncFun}
+module.exports = { asyncFun }
 
 // 测试
 // const test = async () => {
 //     const delay = (Math.random() * 1000).toFixed()
 //     await sleep(delay)
 // }
-// setTimeout(() => asyncFun(test, 4, 200))
+// setTimeout(() => asyncFun(test, 4, 20))
