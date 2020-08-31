7327ea6017cd862915ec145511fe7256bf74bfae
xiaran <xiaran@polomi.com>
2020-08-31 09:44:27 +0800

    update 06

--- node/06/cookie.js
+++ node/06/cookie.js
@@ -1,25 +1,36 @@
 const http = require('http')
 const session = {}
 http.createServer((req, res) => {
-    const sessionKey = 'sid'
-
     if (req.url === '/favicon.ico') {
+        res.end('')
         return
+    }
+    // 观察
+    // console.log('cookie',req.headers.cookie)
+
+    // // 设置Cookie
+    // res.setHeader('Set-Cookie','cookie1=abc;')
+    // res.end('Hello cookie')
+    const sessionKey = 'sid'
+    const cookie = req.headers.cookie
+    if (cookie && cookie.indexOf(sessionKey) > -1) {
+        res.end('Come back')
+        const pattern = new RegExp(`${sessionKey}=([^;]+);?\s*`)
+        const sid = pattern.exec(cookie)[1]
+        console.log('session:', sid, session)
     } else {
-        const cookie = req.headers.cookie
-        if (cookie && cookie.indexOf(sessionKey) > -1) {
-            res.end('Come Back')
-            console.log('cookie:', req.headers.cookie)
-            // 简略写法未必具有通用性
-            const pattern = new RegExp(`${sessionKey}=([^;]+);?\s*`)
-            const sid = pattern.exec(cookie)[1]
-            console.log('session:', sid, session, session[sid])
-        } else {
-            const sid = (Math.random() * 9999999).toFixed()
-            res.setHeader('Set-Cookie', `${sessionKey}=${sid}`)
-            session[sid] = { name: 'laowang' }
-            res.end('hello cookie')
+        // 首次登陆
+        const sid = (Math.random() * 999999).toFixed()
+        // uuid
+        // 设置sid
+        res.setHeader('Set-Cookie', `${sessionKey}=${sid}`)
+        session[sid] = {
+            name: 'laowang'
         }
+        res.end('Hello')
+
     }
 
-}).listen(3000)
+
+})
+    .listen(3000)
--- node/06/redis.js
+++ node/06/redis.js
@@ -1,7 +1,9 @@
 const redis = require('redis')
+
 const client = redis.createClient(6379,'localhost')
 
-client.set('hello','hahaha')
-client.get('hello',function(err,v){
-    console.log('redis key:',v)
+client.set('hello','This is 20 6666')
+
+client.get('hello', function(err,v) {
+    console.log('redis get', v)
 })
