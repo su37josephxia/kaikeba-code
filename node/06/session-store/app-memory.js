c4c0ae1db01760a5b92dd5edf41cb607fc61ccb2
xiaran <xiaran@polomi.com>
2020-09-01 21:40:34 +0800

    update 07 01

--- node/06/session-store/app-memory.js
+++ node/06/session-store/app-memory.js
@@ -2,13 +2,6 @@ const koa = require('koa')
 const app = new koa()
 const session = require('koa-session')
 
-const redisStore = require('koa-redis')
-const redis = require('redis')
-const redisClient = redis.createClient(6379, 'localhost')
-
-const wrapper = require('co-redis')
-const client = wrapper(redisClient)
-
 app.keys = ['some secret']
 
 class Memory {
