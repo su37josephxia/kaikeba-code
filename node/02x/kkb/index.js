26118029a6092bce8544b3e64cb27dbafea51079
josephxia <15906475@qq.com>
2021-08-20 11:14:33 +0800

    update 32

--- node/02x/kkb/index.js
+++ node/02x/kkb/index.js
@@ -9,10 +9,47 @@
 
 const KKB = require("./kkb");
 const app = new KKB();
-app.use((req, res) => {
-  res.writeHead(200);
-  res.end("Hi kaikeba");
+
+
+const Router = require('./router') 
+const router = new Router();
+
+// 设置策略
+router.get('/index', async ctx => { 
+  console.log('index,xx')
+  ctx.body = 'index page'; 
 });
+router.get('/post', async ctx => { ctx.body = 'post page'; });
+router.get('/list', async ctx => { ctx.body = 'list page'; });
+router.post('/index', async ctx => { ctx.body = 'post page'; });
+
+// 路由实例输出父中间件 router.routes()
+app.use(router.routes());
+
+
+// app.use((req, res) => {
+//   res.writeHead(200);
+//   res.end("Hi kaikeba");
+// });
+// const delay = () => new Promise(resolve => setTimeout(() => resolve() ,2000));
+
+// app.use(async (ctx, next) => {
+//   ctx.body = "1";
+//   await next();
+//   ctx.body += "5";
+// });
+
+// app.use(async (ctx, next) => {
+//   ctx.body += "2";
+//   await delay();
+//   await next();
+//   ctx.body += "4";
+// });
+
+// app.use(async (ctx, next) => {
+//   ctx.body += "3";
+// });
+
 app.listen(3000, () => {
   console.log("Server at 3000");
 });
--- node/02x/kkb/kkb.js
+++ node/02x/kkb/kkb.js
@@ -1,14 +1,70 @@
-const http = require('http')
+const http = require("http");
+const context = require("./context");
+const request = require("./request");
+const response = require("./response");
+
 class KKB {
-    listen(...args) {
-        // 启动http服务
-        const server = http.createServer((req,res) => {
-            this.callback(req,res)
-        })
-        server.listen(...args)
-    }
-    use(callback) {
-        this.callback = callback
-    }
+  constructor() {
+    this.middlewares = [];
+  }
+  listen(...args) {
+    // 启动http服务
+    const server = http.createServer(async (req, res) => {
+      const ctx = this.createContext(req, res);
+
+      //   this.callback(ctx);
+      // 中间件合成
+      const fn = this.compose(this.middlewares);
+      await fn(ctx);
+      // ctx.body
+
+      // 响应
+      res.end(ctx.body);
+    });
+    server.listen(...args);
+  }
+  //   use(callback) {
+  //     this.callback = callback;
+  //   }
+  use(middleware) {
+    this.middlewares.push(middleware);
+  }
+
+  /**
+   * 创建上下文
+   * @param {*} req
+   * @param {*} res
+   */
+  createContext(req, res) {
+    const ctx = Object.create(context);
+    ctx.request = Object.create(request);
+    ctx.response = Object.create(response);
+
+    ctx.req = ctx.request.req = req;
+    ctx.res = ctx.response.res = res;
+
+    return ctx;
+  }
+  /**
+   * 合成函数
+   * @param {*} middlewares
+   * @returns
+   */
+  compose(middlewares) {
+    return function (ctx) {
+      return dispatch(0);
+      function dispatch(i) {
+        const fn = middlewares[i];
+        if (!fn) {
+          return Promise.resolve();
+        }
+        return Promise.resolve(
+          fn(ctx, function next() {
+            return dispatch(i + 1);
+          })
+        );
+      }
+    };
+  }
 }
-module.exports = KKB
+module.exports = KKB;
